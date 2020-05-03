52ba12dcaa517a568430153a06f2282a1b22ca45
hashmicurl <tpsystemsarchitect@gmail.com>
2020-05-02 23:01:41 -0400

    feture branch init remote

---
+++ src/main/java/com/example/sprinbootmvndevops/SprinbootMvnDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootmvndevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootMvnDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootMvnDevopsApplication.class, args);
+	}
+
+}
